79c56dc5d036ccb5fb0c1e3f3296601f31d7b1d4
Harry Mishinev <harry@mishinev.com>
2020-06-22 22:43:02 +0300

    Initial commit

---
+++ YouTubeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouTubeApp
+//
+//  Created by Harry Mishinev on 22.06.20.
+//  Copyright © 2020 Harry Mishinev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
